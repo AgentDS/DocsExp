aa9f914bf2575592aaad52b190b2bc67f4578954
AgentDS <zszxlsq@gmail.com>
2021-03-13 11:33:42 +0800

    add setup script

--- setup.py
+++ setup.py
@@ -4,3 +4,28 @@
 # @Contact : zszxlsq@gmail.com
 # @File    : setup.py
 # @Software: PyCharm
+from setuptools import setup, find_packages
+
+setup(
+    name="DocsExp",
+    version="0.1",
+    author="AgentDS",
+    author_email="zszxlsq@gmail.com",
+    description="a demo for docs development and package management",
+    url="https://github.com/AgentDS/DocsExp",
+    packages=find_packages(exclude=()),  # TODO: add things in 'exclude'
+    install_requires=['pytorch==1.8.0',
+                      'torchvision==0.9.0',
+                      'numpy'],
+    python_requires='>=3.6',
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+)
