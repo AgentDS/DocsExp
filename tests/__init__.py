57ebdad2fbaf77a0ae5f70b1d549caa8ed53697f
AgentDS <zszxlsq@gmail.com>
2021-03-18 22:37:51 +0800

    ..

---
+++ tests/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Time    : 3/18/21 1:35 PM
+# @Author  : Siqi Liang
+# @Contact : zszxlsq@gmail.com
+# @File    : __init__.py
+# @Software: PyCharm
+
+import unittest
+
+
+def get_tests():
+    # from .resource import ResourceTestCase
+    # from .serializer import ResourceTestCase as SerializerTestCase
+    # from .utils import UtilsTestCase
+    #
+    # resourcesuite = unittest.TestLoader().loadTestsFromTestCase(ResourceTestCase)
+    # serializersuite = unittest.TestLoader().loadTestsFromTestCase(SerializerTestCase)
+    # utilssuite = unittest.TestLoader().loadTestsFromTestCase(UtilsTestCase)
+    #
+    # return unittest.TestSuite([resourcesuite, serializersuite, utilssuite])
+
+    pass
