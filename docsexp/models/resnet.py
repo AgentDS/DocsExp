a518a146dc4b04e3700282b4154a86cae1edd68d
AgentDS <zszxlsq@gmail.com>
2021-04-11 00:15:25 +0800

    add daily notes

--- docsexp/models/resnet.py
+++ docsexp/models/resnet.py
@@ -16,100 +16,49 @@ def conv3x3(in_channels: int, out_channels: int, stride: int = 1) -> torch.nn.Mo
     return nn.Conv2d(in_channels, out_channels, kernel_size=3,
                      stride=stride, padding=1, bias=False)
 
+def calculate():
+    print('this is empty')
 
 # Residual block
-class ResidualBlock(nn.Module):
-    """Create a single residual block
+# class ResidualBlock(nn.Module):
+#     """Create a single residual block
 
-    Initialization for a single residual block.
+#     Initialization for a single residual block.
 
-    Args:
-        in_channels (int):
-        out_channels (int):
-        stride (int):
-        downsample (optinal): Defaults to None.
-
-    Returns:
-
-    Raises:
-        ClickException: The HTTP request failed or the HTTP response contained an invalid body.
-    """
-
-    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
-        super(ResidualBlock, self).__init__()
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
-        self.bn1 = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
-        self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = downsample
-
-    def forward(self, x):
-        residual = x
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
-        if self.downsample:
-            residual = self.downsample(x)
-        out += residual
-        out = self.relu(out)
-        return out
-
-
-# ResNet
-# class ResNet(nn.Module):
-#     """Return a random page.
-#
-#     Performs a GET request to the /page/random/summary endpoint.
-#
 #     Args:
-#         block (torch.nn.Module): net block.
-#         layers (list): number of blocks for each layer
-#         num_classes (int): number of classifier's output classes
-#
-#     Example:
-#         >>> from docsexp.models import ResNet, ResidualBlock
-#         >>> model = ResNet(ResidualBlock, [2, 2, 2])
+#         in_channels (int):
+#         out_channels (int):
+#         stride (int):
+#         downsample (optinal): Defaults to None.
+
+#     Returns:
+
+#     Raises:
+#         ClickException: The HTTP request failed or the HTTP response contained an invalid body.
 #     """
-#
-#     def __init__(self, block: nn.Module, layers: list, num_classes: int = 10):
-#         super(ResNet, self).__init__()
-#         self.in_channels = 16
-#         self.conv = conv3x3(3, 16)
-#         self.bn = nn.BatchNorm2d(16)
+
+#     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+#         super(ResidualBlock, self).__init__()
+#         self.conv1 = conv3x3(in_channels, out_channels, stride)
+#         self.bn1 = nn.BatchNorm2d(out_channels)
 #         self.relu = nn.ReLU(inplace=True)
-#         self.layer1 = self.make_layer(block, 16, layers[0])
-#         self.layer2 = self.make_layer(block, 32, layers[1], 2)
-#         self.layer3 = self.make_layer(block, 64, layers[2], 2)
-#         self.avg_pool = nn.AvgPool2d(8)
-#         self.fc = nn.Linear(64, num_classes)
-#
-#     def make_layer(self, block: torch.nn.Module, out_channels: int, blocks: int, stride: int = 1):
-#         downsample = None
-#         if (stride != 1) or (self.in_channels != out_channels):
-#             downsample = nn.Sequential(
-#                 conv3x3(self.in_channels, out_channels, stride=stride),
-#                 nn.BatchNorm2d(out_channels))
-#         layers = []
-#         layers.append(block(self.in_channels, out_channels, stride, downsample))
-#         self.in_channels = out_channels
-#         for i in range(1, blocks):
-#             layers.append(block(out_channels, out_channels))
-#         return nn.Sequential(*layers)
-#
+#         self.conv2 = conv3x3(out_channels, out_channels)
+#         self.bn2 = nn.BatchNorm2d(out_channels)
+#         self.downsample = downsample
+
 #     def forward(self, x):
-#         out = self.conv(x)
-#         out = self.bn(out)
+#         residual = x
+#         out = self.conv1(x)
+#         out = self.bn1(out)
 #         out = self.relu(out)
-#         out = self.layer1(out)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.avg_pool(out)
-#         out = out.view(out.size(0), -1)
-#         out = self.fc(out)
+#         out = self.conv2(out)
+#         out = self.bn2(out)
+#         if self.downsample:
+#             residual = self.downsample(x)
+#         out += residual
+#         out = self.relu(out)
 #         return out
+
 
 class MyClass(object):
     """Test for MyClass
