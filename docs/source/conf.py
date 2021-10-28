442cd91520ee3eedefe8370a3e5d86b716223f41
AgentDS <zszxlsq@gmail.com>
2021-10-28 19:18:12 +0800

    ..

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,6 +100,9 @@ pygments_style = 'sphinx'
 # If true, 'todo' and 'todoList' produce output, else they produce nothing.
 todo_include_todos = True
 
+# -- Multi-languages docs  --------------------------------------------------
+language = 'en'
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
